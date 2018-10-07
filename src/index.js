c7bafd0ec6cdd33f02c7fce945939378589e4d83
Jean-Philippe Sirois <jp@veksen.net>
2018-10-07 17:40:05 -0400

    chore: setup styled-components & remove basic styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles.js";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
